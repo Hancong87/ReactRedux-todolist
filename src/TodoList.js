b0c966cb44af7adc8ed8ebb346e2d4748b6b726d
hancong <hancong.guan@mastercard.com>
2018-08-29 16:13:10 +0800

    react-redux part 2

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react';
 import store from './store';
+import {connect} from 'react-redux';
 
 class TodoList extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
-    }
 
     render() {
         return (
             <div>
                 <div>
-                    <input value={this.state.inputValue}/>
+                    <input value={this.props.inputValue} onChange={this.props.changeInputChange}/>
                     <button>submit</button>
                 </div>
                 <ul>
@@ -21,6 +17,30 @@ class TodoList extends Component {
             </div>
         );
     }
+
+    handleInputChange(e) {
+        console.log(e.target.value);
+    }
 }
 
-export default TodoList;
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue
+    }
+};
+
+// store.dispatch, props
+const mapDispatchToProps = (dispatch) => {
+  return {
+      changeInputChange(e) {
+            const action = {
+                type:'change_input_value',
+                value: e.target.value
+            };
+            dispatch(action);
+      }
+  }
+};
+
+//让TodoList和store做连接
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList';
+import {Provider} from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+// Provider使得每一个内部组件，都可以使用store
+const App = (
+    <Provider store={store}>
+        <TodoList/>
+    </Provider>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
